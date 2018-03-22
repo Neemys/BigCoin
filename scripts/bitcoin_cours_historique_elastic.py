368bbf95b31de2142b98af88ce127ed95f165df2
Stéphane <stephane.tiv@gmail.com>
2018-03-22 14:04:14 +0100

    Fix host and port parameters in python script

--- scripts/bitcoin_cours_historique_elastic.py
+++ scripts/bitcoin_cours_historique_elastic.py
@@ -4,6 +4,7 @@ from elasticsearch import helpers
 from datetime import date
 import datetime
 import json
+import sys
 
 def add_elastic():
 	for i in data["bpi"]:
@@ -18,11 +19,13 @@ def add_elastic():
 			}
 		}
 
+host = sys.argv[1]
+port = sys.argv[2]
 # Retrieve data from API
 url_cours_bitcoin = 'https://api.coindesk.com/v1/bpi/historical/close.json?start=2011-01-01&end=2018-03-20'
 res = requests.get(url_cours_bitcoin)
 data = res.json()
 # Connect to elastic search
-es = Elasticsearch([{'host': 'localhost', 'port':9200}])
+es = Elasticsearch([{'host': host, 'port': port}])
 # Add data in elastic search
 helpers.bulk(es, add_elastic())
